d92d090e7b7a8121c8177d4ae382470509a40461
Carson Anderson <carson.anderson@getweave.com>
2024-12-10 07:40:49 -0700

    feat: add day 10a

---
+++ d10a/app.py
@@ -0,0 +1,31 @@
+import sys
+import collections
+from .gridlib.gridlib import Grid
+
+grid = Grid(filepath=sys.argv[1])
+grid.print()
+
+
+def search(cell, ends):
+    match cell.value:
+        case "9":
+            ends[cell.id] = ends[cell.id] + 1
+
+    for n in grid.cardinal_cells(cell):
+        if n.value == "." or cell.value == ".":
+            continue
+        if int(n.value) - int(cell.value) == 1:
+            ends = search(n, ends)
+
+    return ends
+
+
+sum = 0
+for cell in grid:
+    if cell.value != "0":
+        continue
+    ends = search(cell, collections.defaultdict(int))
+    print(ends)
+    sum += len(ends)
+
+print(sum)
