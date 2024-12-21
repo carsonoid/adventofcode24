3952a67dc5ef62d2bfb5157dbff7e1aae5b4a453
Carson Anderson <carson.anderson@getweave.com>
2024-12-21 14:17:45 -0700

    chore: keep trying 21

--- d21a/app.py
+++ d21a/app.py
@@ -2,91 +2,112 @@ import sys
 from .gridlib.gridlib import Grid, Cell
 
 
+class Arrowpad:
+    name: str
+    grid: Grid
+    keys: dict
+    pos: Cell
+
+    def __init__(self, name):
+        self.name = name
+        self.grid = Grid(s=".^A\n<v>")
+        self.keys = {}
+        for cell in self.grid:
+            self.keys[cell.value] = cell
+        self.pos = self.keys["A"]
+
+    def press(self, key):
+        next = self.keys[key]
+        x = next.x - self.pos.x
+        y = next.y - self.pos.y
+        keys = []
+        if x < 0:
+            keys.extend(["<"] * abs(x))
+        elif x > 0:
+            keys.extend([">"] * x)
+
+        if y < 0:
+            keys.extend(["^"] * abs(y))
+        elif y > 0:
+            keys.extend(["v"] * y)
+
+        keys.append("A")
+
+        self.pos = next
+
+        return keys
+
+    def press_all(self, keys):
+        all = []
+        for key in keys:
+            all.extend(self.press(key))
+        return all
+
+
 class Numpad:
     name: str
     grid: Grid
     keys: dict
     pos: Cell
 
-    def __init__(self, name, grid: Grid, parent=None):
+    def __init__(self, name):
         self.name = name
-        self.grid = grid
+        self.grid = Grid(s="789\n456\n123\n.0A")
         self.keys = {}
-        for cell in grid:
+        for cell in self.grid:
             self.keys[cell.value] = cell
         self.pos = self.keys["A"]
-        self.parent = parent
 
-    def press(self, key, times=1):
+    def press(self, key):
         next = self.keys[key]
         x = next.x - self.pos.x
         y = next.y - self.pos.y
-        keys = [key] * times
-        print(f"{self.name} - next: {x} {y}")
+        keys = []
+        if x < 0:
+            keys.extend(["<"] * abs(x))
+        elif x > 0:
+            keys.extend([">"] * x)
 
-        if self.parent:
-            if x < 0:
-                _, _, pkeys = self.parent.press("<", times=abs(x))
-                keys.extend(pkeys)
-            elif x > 0:
-                _, _, pkeys = self.parent.press(">", times=x)
-                keys.extend(pkeys)
-            else:
-                _, _, pkeys = self.parent.press("A")
-                keys.extend(pkeys)
+        if y < 0:
+            keys.extend(["^"] * abs(y))
+        elif y > 0:
+            keys.extend(["v"] * y)
 
-            _, _, pkeys = self.parent.press("A")
-            keys.extend(pkeys)
-
+        keys.extend("A")
         self.pos = next
 
-        return x, y, keys
+        return keys
 
-
-numpad_str = "789\n456\n123\n.0A"
-arrowpad_str = ".^A\n<v>"
+    def press_all(self, keys):
+        all = []
+        for key in keys:
+            all.extend(self.press(key))
+        return all
 
 
 with open(sys.argv[1]) as f:
     codes = f.read().strip().split("\n")
 
-r1 = Numpad("r1", Grid(s=arrowpad_str))
-dp = Numpad("dp", Grid(s=numpad_str), r1)
 
-print(dp.press("0"))
-# print(dp.press("2"))
-# print(dp.press("9"))
-# print(dp.press("A"))
-# tot = 0
-# for k in "029A":
-#     _, _, c = dp.press(k)
-#     print(k, "cost", c)
-#     tot += c
-#     break
+tot = 0
+codes = ["456A"]
+for code in codes:
+    dp = Numpad("dp")
+    ap1 = Arrowpad("ap1")
+    ap2 = Arrowpad("ap2")
 
-# print(tot)
+    print(code)
+    l1 = dp.press_all(code)
+    print("".join(l1))
 
-# for code in codes:
-#     print(code)
-#     dp = Numpad(Grid(s=numpad_str))
-#     r1 = Numpad(Grid(s=arrowpad_str), dp)
-#     r2 = Numpad(Grid(s=arrowpad_str), r1)
-#     hp = Numpad(Grid(s=arrowpad_str), r2)
+    l2 = ap1.press_all("".join(l1))
+    print("".join(l2))
 
-#     for key in code:
-#         dpv, dph = dp.press(key)
-#         print(f"dp: {dpv} {dph}")
-#         if dph < 0:
-#             rh, rv = r1.press("<")
-#             print(f"r1: {rv} {rh}")
-#         if dph > 0:
-#             rh, rv = r1.press(">")
-#             print(f"r1: {rv} {rh}")
-#         if dpv < 0:
-#             rh, rv = r1.press("^")
-#             print(f"r1: {rv} {rh}")
-#         if dpv > 0:
-#             rh, rv = r1.press("v")
-#             print(f"r1: {rv} {rh}")
-#         break
-#     break
+    l3 = ap2.press_all("".join(l2))
+    print("".join(l3))
+
+    complexity = len(l3) * int(code.rstrip("A"))
+    print(len(l3), int(code.rstrip("A")), complexity)
+    tot += complexity
+
+print(tot)
